1f64bdf490d960347dba526cd51326fff4b66373
user <user@user-Aspire-VN7-572G>
2020-08-05 21:06:43 +0200

    if

--- src/main/clojure/hello.clj
+++ src/main/clojure/hello.clj
@@ -5,7 +5,12 @@
       :main false
       :methods [[greet [String] void]])
 
-(defn hello [who] (str "Hello " who))
+(defn language ([lang] (if (= lang "de") "Hallo" "Hello"))
+  ([] (str "Hello")))
+
+(defn hello ([who] (str (language) " " who))
+  ([lang who] (str (language lang) " " who)))
 
 (defn -greet [this who] (println (hello who)))
 
+ 
--- src/test/clojure/tests.clj
+++ src/test/clojure/tests.clj
@@ -3,3 +3,8 @@
 (-greet nil "Soeren")
 
 (assert (= (hello "World") "Hello World"))
+(assert (= (hello "de" "Welt") "Hallo Welt"))
+
+(assert (= (language "de") "Hallo"))
+(assert (= (language "en") "Hello"))
+(assert (= (language) "Hello"))
